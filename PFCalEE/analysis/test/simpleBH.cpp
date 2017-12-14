ff5bf051c6c04e9569170f489433fac0ccb585de
Sarah <eno@umd.edu>
2017-12-14 13:30:28 -0500

    ha

--- PFCalEE/analysis/test/simpleBH.cpp
+++ PFCalEE/analysis/test/simpleBH.cpp
@@ -325,7 +325,8 @@ int main(int argc, char** argv){//main
   TH2F* h_EpPhi = new TH2F("h_EpPhi","Ereco/gen versus phi",100,-4.,4.,100,0.,20.);
   TH2F* h_etagenmax= new TH2F("h_etagenmax","eta gen vs max",100,1.,5.,100,1.,5.);
   TH2F* h_phigenmax= new TH2F("h_phigenmax","phi gen vs max",100,-4,4.,100,-4.,4.);
-  TH1F* h_maxE = new TH1F("h_maxE","energy of highest energy hit",1000,0.,1000.);
+  TH1F* h_maxE = new TH1F("h_maxE","energy of highest energy hit",1000,0.,5000.);
+  TH1F* h_ECone03 = new TH1F("h_ECone03","Sum energy cone 03",1000,0.,5000.);
   
   ///////////////////////////////////////////////////////
   //////////////////  start event loop
@@ -581,6 +582,8 @@ int main(int argc, char** argv){//main
     h_EpCone->Fill(0.4,rechitsumE04/Egen);
     h_EpCone->Fill(0.5,rechitsumE05/Egen);
     
+
+    h_ECone03->Fill(rechitsumE03);
       //miptree->Fill();
 
     geomConv.initialiseHistos();
